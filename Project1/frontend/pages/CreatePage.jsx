e0c97be1b2f595ea3f2fd67c7fc15eed6156b54e
Shivanshu Singh <shivanshusingh71@gmail.com>
2024-08-29 02:40:31 +0530

    "frontend started "

---
+++ Project1/frontend/pages/CreatePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CreatePage = () => {
+  return (
+    <>
+        <h1>Espresso</h1>
+    </>
+  )
+}
+
+export default CreatePage
---
+++ Project1/frontend/vite-project/src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react"
+import { Button , Box } from "@chakra-ui/react"
+import {Route,Routes} from 'react-router-dom'
+import CreatePage from "../../pages/CreatePage"
+import HomePage from "../../pages/HomePage"
+import Navbar from "../../components/Navbar"
+
+function App() {
+
+
+  return (
+    <>
+      <Box minH={"100vh"}>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<HomePage/>} />
+          <Route path="/create" element={<CreatePage/>} />
+        </Routes>
+        </Box>
+    </>
+  )
+}
+
+export default App
